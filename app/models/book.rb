54237f55cd4bbef15dcebbc2dc9475cd7feb8194
EC2 Default User <ec2-user@ip-172-31-4-103.ap-northeast-1.compute.internal>
2023-10-07 06:25:20 +0000

    [Finish]応用課題8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -20,6 +20,15 @@ class Book < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
   
+  #7日間の記録
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+    scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+    scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) }
+    scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) }
+    scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) }
+    scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) }
+    scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) }
+  
   #記録機能
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
     scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
